2a27964d094c51a2def40bd7a7a9753aa47ef5e6
chenxing <mitrecx@163.com>
2021-04-26 00:45:57 +0800

    15. 3Sum

--- src/main/java/cn/mitrecx/LeetCode15.java
+++ src/main/java/cn/mitrecx/LeetCode15.java
@@ -4,46 +4,50 @@ import java.util.*;
 
 public class LeetCode15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums.length < 3) {
-            return result;
-        }
-        for (int i = 2; i < nums.length; i++) {
-            int expectResult = -nums[i];
-            Map<Integer, Integer> valueCount = new HashMap<>();
 
-            List<Integer> tuple = new ArrayList<>();
-            for (int j = 0; j < i; j++) {
-                int expect = expectResult - nums[j];
-                if (valueCount.containsKey(expect)) {
-                    tuple.add(nums[i]);
-                    tuple.add(nums[j]);
-                    tuple.add(expect);
-                    if (!contains(result, tuple)) {
-                        result.add(new ArrayList<>(tuple));
+//        nums = Arrays.stream(nums).sorted().toArray();
+        List<List<Integer>> results = new ArrayList<>();
+        if (nums == null || nums.length == 0)
+            return results;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            // 相同的元素，只要fix一次就行了，这样可以保证三元组不重复
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int start = i + 1;
+            int end = nums.length - 1;
+            int twoSum = 0 - nums[i];
+            while (start < end) {
+                int oneSum = twoSum - nums[start];
+                if (oneSum == nums[end]) {
+                    List<Integer> r = new ArrayList<>();
+                    r.add(nums[i]);
+                    r.add(nums[start]);
+                    r.add(nums[end]);
+                    results.add(r);
+                    start++;
+                    end--;
+                    // 如果start和上一个元素相同，start后移，保证三元组不重复（唯一性）
+                    while (start < end && nums[start - 1] == nums[start]) {
+                        start++;
                     }
-                    tuple.clear();
+                } else if (nums[end] > oneSum) {
+                    end--;
+                } else {
+                    start++;
                 }
-                valueCount.put(nums[j], j);
             }
         }
-        return result;
+        return results;
     }
 
-    public boolean contains(List<List<Integer>> result, List<Integer> tuple) {
-        return result.stream().anyMatch(r -> {
-            r.sort((a, b) -> a > b ? -1 : 1);
-            tuple.sort((a, b) -> a > b ? -1 : 1);
-            if (r.get(0).equals(tuple.get(0)) && r.get(1).equals(tuple.get(1)) && r.get(2).equals(tuple.get(2))) {
-                return true;
-            }
-            return false;
-        });
-    }
 
     public static void main(String[] args) {
         LeetCode15 leetCode15 = new LeetCode15();
-        int[] nums = {-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
+//        int[] nums = {-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
+        int[] nums = {-1, 0, 1, 2, -1, -4};
         System.out.println(leetCode15.threeSum(nums));
     }
 }
