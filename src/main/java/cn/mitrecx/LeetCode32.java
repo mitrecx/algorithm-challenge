a0286bad2943e86707306d6ffc8a90a304ca1c55
chenxing <mitrecx@163.com>
2022-01-17 23:35:46 +0800

    32. Longest Valid Parentheses

---
+++ src/main/java/cn/mitrecx/LeetCode32.java
@@ -0,0 +1,34 @@
+package cn.mitrecx;
+
+import java.util.LinkedList;
+
+public class LeetCode32 {
+    public static void main(String[] args) {
+        LeetCode32 leetCode32 = new LeetCode32();
+        System.out.println(leetCode32.longestValidParentheses("(()"));
+        System.out.println(leetCode32.longestValidParentheses(")()())"));
+    }
+
+    // 用栈存下标!
+    // 栈底元素存 "最后一个没有匹配的右括号的下标"; 初始化栈顶元素为 -1
+    // 遍历字符串, 是左括号时, 入栈;
+    // 是右括号时, 出栈
+    public int longestValidParentheses(String s) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i); //栈底元素存 "最后一个没有匹配的右括号的下标"
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
