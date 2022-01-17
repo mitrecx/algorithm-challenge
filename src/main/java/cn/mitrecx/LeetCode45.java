d6cfa8d6c9650b1e772ed96a6c62d376bb768ad2
chenxing <mitrecx@163.com>
2022-01-18 00:11:08 +0800

    45. Jump Game II

---
+++ src/main/java/cn/mitrecx/LeetCode45.java
@@ -0,0 +1,24 @@
+package cn.mitrecx;
+
+public class LeetCode45 {
+    public static void main(String[] args) {
+        LeetCode45 leetCode45 = new LeetCode45();
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(leetCode45.jump(nums));
+    }
+
+    public int jump(int[] nums) {
+        int maxPosition = 0;
+        // 当前轮的最后一个元素下标
+        int currentRoundEnd = 0;
+        int steps = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == currentRoundEnd) {
+                steps++;
+                currentRoundEnd = maxPosition;
+            }
+        }
+        return steps;
+    }
+}
