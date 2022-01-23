fbdcd6303fd3581087c192ebad7d4e1a6be23c8d
chenxing <mitrecx@163.com>
2022-01-23 21:25:25 +0800

    56. Merge Intervals

---
+++ src/main/java/cn/mitrecx/LeetCode56.java
@@ -0,0 +1,30 @@
+package cn.mitrecx;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 56. Merge Intervals
+ * https://leetcode.com/problems/merge-intervals/
+ */
+public class LeetCode56 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int left = intervals[i][0], right = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < left) {
+                merged.add(new int[]{left, right});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], right);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+}
