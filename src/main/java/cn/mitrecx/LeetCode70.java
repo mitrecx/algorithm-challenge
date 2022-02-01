ad4dcceccd5d589f24e2fd99de7198af9dd43910
Xing Chen <mitrechen@borderxlab.com>
2022-02-01 11:35:51 +0800

    70. Climbing Stairs

---
+++ src/main/java/cn/mitrecx/LeetCode70.java
@@ -0,0 +1,35 @@
+package cn.mitrecx;
+
+/**
+ * 70. Climbing Stairs
+ * <p>
+ * 动态规划:
+ * f(x) = f(x-1) + f(x-2)
+ */
+public class LeetCode70 {
+    public static void main(String[] args) {
+        LeetCode70 leetCode70 = new LeetCode70();
+        System.out.println(leetCode70.climbStairs(2));
+        System.out.println(leetCode70.climbStairs(3));
+        System.out.println(leetCode70.climbStairs(4));
+    }
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int firstToLast = 2, secondToLast = 1;
+        for (int i = 3; i <= n; i++) {
+            int temp = firstToLast;
+            firstToLast += secondToLast;
+            secondToLast = temp;
+        }
+        return firstToLast;
+    }
+}
