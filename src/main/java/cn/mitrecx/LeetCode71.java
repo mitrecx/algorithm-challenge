713e067c57297d316858aab0fe8298b4d05c554f
Xing Chen <mitrechen@borderxlab.com>
2022-02-06 10:38:01 +0800

    71. Simplify Path

---
+++ src/main/java/cn/mitrecx/LeetCode71.java
@@ -0,0 +1,45 @@
+package cn.mitrecx;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 71. Simplify Path
+ * <p>
+ * 栈
+ */
+public class LeetCode71 {
+    public static void main(String[] args) {
+        LeetCode71 leetCode71 = new LeetCode71();
+        System.out.println(leetCode71.simplifyPath("/home/"));
+        System.out.println(leetCode71.simplifyPath("/../"));
+        System.out.println(leetCode71.simplifyPath("/home//foo/"));
+    }
+
+    public String simplifyPath(String path) {
+        // split
+        String[] pathElement = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+        for (String e : pathElement) {
+            if ("..".equals(e)) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+                continue;
+            }
+            if (!e.isEmpty() && !".".equals(e)) {
+                stack.offerLast(e);
+            }
+        }
+        StringBuilder r = new StringBuilder();
+        if (stack.isEmpty()) {
+            r.append("/");
+        } else {
+            while (!stack.isEmpty()) {
+                r.append("/");
+                r.append(stack.pollFirst());
+            }
+        }
+        return r.toString();
+    }
+}
