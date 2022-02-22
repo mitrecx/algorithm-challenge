7dc8f35755a8ece3f935a66e65d61dec00fc8135
chenxing <mitrecx@163.com>
2022-02-22 22:38:59 +0800

    103. Binary Tree Zigzag Level Order Traversal

--- src/main/java/cn/mitrecx/LeetCode103.java
+++ src/main/java/cn/mitrecx/LeetCode103.java
@@ -6,24 +6,24 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Title: 102. Binary Tree Level Order Traversal
+ * Title: 103. Binary Tree Zigzag Level Order Traversal
  * Difficulty: Medium
- * Self Difficulty: 下
+ * Self Difficulty: 中
  * kw: 二叉树(binary tree)
  */
-public class LeetCode102 {
+public class LeetCode103 {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
-        LeetCode102 leetCode102 = new LeetCode102();
-        List<List<Integer>> ans = leetCode102.levelOrder(root);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        LeetCode103 leetCode103 = new LeetCode103();
+        List<List<Integer>> ans = leetCode103.zigzagLevelOrder(root);
         ans.forEach(System.out::println);
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) {
             return ans;
@@ -33,9 +33,9 @@ public class LeetCode102 {
         queue1.push(root);
         while (!queue1.isEmpty() || !queue2.isEmpty()) {
             if (queue2.isEmpty()) {
-                ans.add(process(queue1, queue2));
+                ans.add(process(queue1, queue2, false));
             } else {
-                ans.add(process(queue2, queue1));
+                ans.add(process(queue2, queue1, true));
             }
         }
         return ans;
@@ -44,17 +44,27 @@ public class LeetCode102 {
     /**
      * @param currQueue      当前处理的queue, 里面存储了某一层的所有结点
      * @param auxiliaryQueue 辅助queue, 存储 currQueue 里结点的孩子结点
+     * @param reverse        标识正逆序, false正序: left->right; true表示逆序: right->left
      */
-    private List<Integer> process(Deque<TreeNode> currQueue, Deque<TreeNode> auxiliaryQueue) {
+    private List<Integer> process(Deque<TreeNode> currQueue, Deque<TreeNode> auxiliaryQueue, boolean reverse) {
         List<Integer> r = new ArrayList<>();
+        Deque<Integer> temp = new LinkedList<>();
         while (!currQueue.isEmpty()) {
             TreeNode curr = currQueue.removeFirst();
-            r.add(curr.val);
+            temp.addLast(curr.val);
             if (curr.left != null) {
                 auxiliaryQueue.addLast(curr.left);
             }
             if (curr.right != null) {
                 auxiliaryQueue.addLast(curr.right);
+            }
+
+        }
+        while (!temp.isEmpty()) {
+            if (reverse) {
+                r.add(temp.removeLast());
+            } else {
+                r.add(temp.removeFirst());
             }
         }
         return r;
