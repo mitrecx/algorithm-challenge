290338a31867a1c574c2d3a888864836bf213ff4
chenxing <mitrecx@163.com>
2022-03-18 00:18:18 +0800

    148. Sort List

---
+++ src/main/java/cn/mitrecx/LeetCode148.java
@@ -0,0 +1,87 @@
+package cn.mitrecx;
+
+/**
+ * Title: 148. Sort List
+ * Difficulty: Medium
+ * Self Difficulty: 上
+ * kw: 单链表, 归并排序, 快慢指针
+ *
+ * @see cn.mitrecx.sort.MergeSort
+ * @see LeetCode21
+ */
+public class LeetCode148 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(-1);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(0);
+        ListNode.print(head);
+        LeetCode148 leetCode148 = new LeetCode148();
+        ListNode.print(leetCode148.sortList(head));
+    }
+
+    public ListNode sortList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode tail = head;
+        while (tail.next != null){
+            tail = tail.next;
+        }
+        return sortList(head, tail);
+    }
+
+    private ListNode sortList(ListNode head, ListNode tail) {
+        if (head == tail) {
+            return head;
+        }
+        // 归并排序
+        ListNode mid = getMiddleNode(head, tail);
+        ListNode midRight = mid.next;
+        mid.next = null;
+
+        ListNode ln1 = sortList(head, mid); // 左边
+
+        ListNode ln2 = sortList(midRight, tail); // 右边
+
+        return merge(ln1, ln2); // 合并
+    }
+
+    /**
+     * @see LeetCode143#getMiddleNode(cn.mitrecx.ListNode)
+     */
+    public ListNode getMiddleNode(ListNode head, ListNode tail) { // 左中
+        ListNode slow = head, fast = head;
+        while (fast != tail && fast.next != tail) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * @see LeetCode21#mergeTwoLists(cn.mitrecx.ListNode, cn.mitrecx.ListNode)
+     */
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode();
+        ListNode temp = dummyHead;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        if (head1 != null) {
+            temp.next = head1;
+        }
+        if (head2 != null) {
+            temp.next = head2;
+        }
+        return dummyHead.next;
+    }
+}
--- src/main/java/cn/mitrecx/LeetCode21.java
+++ src/main/java/cn/mitrecx/LeetCode21.java
@@ -1,10 +1,12 @@
 package cn.mitrecx;
 
 /**
- * 21. Merge Two Sorted Lists
+ * Title: 21. Merge Two Sorted Lists
+ * Difficulty: Easy
+ * Self Difficulty:
+ * kw: 单链表
  */
 public class LeetCode21 {
-
     public static void main(String[] args) {
         LeetCode21 leetCode21 = new LeetCode21();
         ListNode a = new ListNode(4);
@@ -12,49 +14,40 @@ public class LeetCode21 {
         b.next = a;
         ListNode list1 = new ListNode(1);
         list1.next = b;
-        leetCode21.print(list1);
+        ListNode.print(list1);
 
         ListNode c = new ListNode(4);
         ListNode d = new ListNode(3);
         d.next = c;
         ListNode list2 = new ListNode(1);
         list2.next = d;
-        leetCode21.print(list2);
+        ListNode.print(list2);
 
         ListNode r = leetCode21.mergeTwoLists(list1, list2);
-        leetCode21.print(r);
-
+        ListNode.print(r);
     }
 
-    private void print(ListNode node) {
-        while (node != null) {
-            System.out.print(node.val + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode r = new ListNode();
-        ListNode head = r;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                r.next = list1;
-                r = r.next;
-                list1 = list1.next;
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode();
+        ListNode temp = dummyHead;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
             } else {
-                r.next = list2;
-                r = r.next;
-                list2 = list2.next;
+                temp.next = head2;
+                head2 = head2.next;
             }
+            temp = temp.next;
         }
-        if (list1 != null) {
-            r.next = list1;
+        if (head1 != null) {
+            temp.next = head1;
         }
-        if (list2 != null) {
-            r.next = list2;
+        if (head2 != null) {
+            temp.next = head2;
         }
-        return head.next;
+        return dummyHead.next;
     }
 
 }
