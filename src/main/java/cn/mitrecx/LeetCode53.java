c8975ebaed55d63b4ee025d22f0e5a0e76389f2a
chenxing <mitrecx@163.com>
2022-03-21 00:42:59 +0800

    152. Maximum Product Subarray

--- src/main/java/cn/mitrecx/LeetCode53.java
+++ src/main/java/cn/mitrecx/LeetCode53.java
@@ -1,7 +1,11 @@
 package cn.mitrecx;
 
 /**
- * 53. Maximum Subarray
+ * Title: 53. Maximum Subarray
+ * Difficulty: Easy
+ * Self Difficulty: 下
+ * kw: 动态规划/贪心算法
+ * @see LeetCode152
  */
 public class LeetCode53 {
     public static void main(String[] args) {
@@ -11,14 +15,16 @@ public class LeetCode53 {
         System.out.println(leetCode53.maxSubArray(nums));
     }
 
+    // 动态规划状态转移方程：
+    // f(i) = max( f(i-1) + a[i], a[i] )
+    // 其中，a[i] 表示在 i 处的值，f(i) 表示在 i 处的 最大连续子数组的和
     public int maxSubArray(int[] nums) {
         int max = nums[0];
-        int currSum = max;
-        // 贪心算法
+        // 存 f(i-1)，滚动数组思想
+        int pre = max;
         for (int i = 1; i < nums.length; i++) {
-            // 当前和 取 Math.max(当前值, 之前和+当前值)
-            currSum = Math.max(nums[i], currSum + nums[i]);
-            max = Math.max(currSum, max);
+            pre = Math.max(nums[i], pre + nums[i]);
+            max = Math.max(pre, max);
         }
         return max;
     }
